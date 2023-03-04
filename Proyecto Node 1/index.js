bb0ec68f086364ae2f034b0407e230dae797af84
IgriegaL <yehudilara@hotmail.com>
2023-03-04 14:25:24 -0300

    Servidor Node Nodemon

---
+++ Proyecto Node 1/index.js
@@ -0,0 +1,57 @@
+const http = require('http');
+const cursos = require('./cursos') //se puede agregar .js
+
+const servidor = http.createServer((req, res) => {
+    const {method} = req; // Extraemos de req el metodo
+    switch (method) {
+        case 'GET':
+            return manejarSolicitudGet(req,res)
+        case 'POST':
+            return manejarSolicitudPost(req,res)
+        default:
+            res.statusCode = 501
+            res.end(`El metodo ${method} no puede ser manejado x el servidor..`)
+            break;
+    }
+})
+
+
+function manejarSolicitudGet(req, res){
+    const path = req.url
+    console.log(res.statusCode)
+
+    if (path === '/') {
+        res.writeHead(200, {'contet-type':'application/json'}); // writeHead(CodigoDeEstado, TypoObjeto) 
+        return res.end('Sevidor y api Node')
+    } else if (path === '/cursos'){
+        return res.end(JSON.stringify(cursos.infoCursos)); //Devolvemos los cursos como Json
+    } else if (path === '/cursos/programacion') {
+        return res.end(JSON.stringify(cursos.infoCursos.programacion))
+    } else {
+        res.statusCode = 404;
+        return res.end(JSON.stringify('no existe'))
+    }
+}
+function manejarSolicitudPost(req, res){
+    const path = req.url
+    
+    if (path === '/cursos/programacion') {
+    let cuerpo =''
+    req.on('data', contenido => { // con on y 'data', obtenemos el contenido de la solicitud y lo convertimos en una cadena 
+        cuerpo += contenido.toString();
+    }); 
+    req.on('end', contenido => {
+       console.log(cuerpo) // luego se muestra ese cuerpo
+       console.log(typeof cuerpo)
+       cuerpo = JSON.parse(cuerpo) // Convertir en un obj de Js
+       console.log(typeof cuerpo)
+       res.end('Sevidor recibió tu solicitud Post')
+    }); 
+
+        //return res.end('Sevidor recibió tu solicitud Post')
+    } 
+}
+
+const PUERTO = 3000;
+
+servidor.listen(PUERTO,()=>{console.log(`Servidor escuchando en el Puerto: ${PUERTO}`)});
---
+++ express/app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+// Llamar una app de tipo express
+const app = express();
+
+const { infoCursos } = require("./cursos");
+
+// Routers
+app.use('/api/cursos/programacion', routerProgramacion);
+app.use('/api/cursos/matematicas', routerMatematicas);
+
+// Routing
+
+// Metodo
+app.get("/", (req, res) => {
+  res.send("Mi primer servidor 💻");
+});
+app.get("/api/cursos", (req, res) => {
+  res.send(JSON.stringify(infoCursos));
+});
+
+// Matemática
+
+// :xxxx simboliza que puede ser una variable dinamica
+app.get("/api/cursos/programacion/:lenguaje/:nivel", (req, res) => {
+  const lenguaje = req.params.lenguaje;
+  const nivel = req.params.nivel;
+
+  const resultados = infoCursos.programacion.filter(
+    (curso) => curso.lenguaje === lenguaje && curso.nivel === nivel
+  );
+
+  if (resultados.length === 0) {
+    return res
+      .status(404)
+      .send(`No se encontraron cursos de ${lenguaje} de nivel ${nivel}`);
+  }
+  // Para: localhost:3000/api/cursos/programacion/python?ordenar=vistas
+  if (req.query.ordenar === 'vistas') {
+    return res.send(JSON.stringify(resultados.sort((a,b)=>{
+        b.vistas - a.vistas
+    })))
+  }
+  res.send(JSON.stringify(resultados));
+  
+});
+
+const PUERTO = process.env.PORT || 3000;
+
+app.listen(PUERTO, () => {
+  console.log(`Servidor escuchado ${PUERTO}`);
+});
